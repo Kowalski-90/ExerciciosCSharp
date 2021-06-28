d93c2a5a2a69f4fff58b0ffc98757f8fa0b58d1e
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-06-28 12:50:00 -0300

    Adiciona projeto revisão C# GFT

---
+++ RevisãoGFT/Aluno.cs
@@ -0,0 +1,8 @@
+namespace Revisao
+{
+    public struct Aluno //O ideal é que Aluno fosse uma class. Mas aqui trata-se de um exemplo muito simples, por isso o uso da struct
+    {
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
