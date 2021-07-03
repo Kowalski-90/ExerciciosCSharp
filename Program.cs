050aaf9a020f30987e2506881c4ce7b5f73cac1b
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-03 18:05:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace My_Awesome_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! Tickets are US$ 5. Please insert cash");
+
+            int cash = Convert.ToInt32(Console.ReadLine());
+
+            if(cash < 5)
+            {
+                Console.WriteLine("That's not enough money");
+            }
+            else if (cash == 5)
+            {
+                Console.WriteLine("Here is your ticket");
+            }
+            else
+            {
+                int change = cash -5;
+                Console.WriteLine("Here is your ticket and " + change + " dollars in change");
+            }
+
+            //Wait before closing
+            Console.ReadKey();  
+        }
+    }
+}
