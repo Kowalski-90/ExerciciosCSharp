8f5b4bca1e67e0df6859ff5fd419933f55f553af
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-03 18:39:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace My_Awesome_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1; 
+            int result2; 
+            int result3; 
+            
+
+            Console.WriteLine("What are the results of the math problems below?");
+            Console.WriteLine("15 + 2 * 2 = ");
+            result1 = Convert.ToInt32(Console.ReadLine());
+
+            if(result1 == 19)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong!");
+            }
+            
+            Console.WriteLine("20 * 2 / 4 = ");
+            result1 = Convert.ToInt32(Console.ReadLine());
+
+            if(result1 == 10)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong!");
+            }
+
+            Console.WriteLine("(4 + 3 + 2 + 1) / 2 ");
+            result1 = Convert.ToInt32(Console.ReadLine());
+
+            if(result1 == 5)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong!");
+            }
+
+            //Wait before closing
+            Console.ReadKey();  
+        }
+    }
+}
