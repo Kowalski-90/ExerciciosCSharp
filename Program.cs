c025a2070eee10f11ce9261f35d924f6ac9143e4
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-04 09:23:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace My_Awesome_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numberGen = new Random(); //Define a number generator
+            int roll = 0;
+            int attempts = 0;
+
+            Console.WriteLine("Press enter to roll the dice.");
+
+            while(roll != 6)
+            {
+                Console.ReadKey();//Wait for the user to press the key before doing the loop
+                roll = numberGen.Next(1, 7); //(range of numbers - first is inclusive, second is exclusive, so it goes 1 to 6)
+                Console.WriteLine("You rolled: " + roll);
+                attempts++; 
+            }
+
+            Console.WriteLine("It took you " + attempts + " attempts to roll a six!");
+            //Wait before closing
+            Console.ReadKey();  
+        }
+    }
+}
