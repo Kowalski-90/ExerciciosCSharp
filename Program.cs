a6bbb6edad13bcf411b2f89ba7ed417bdf615457
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-04 09:55:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace My_Awesome_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] movies = new string[4];
+
+            Console.WriteLine("Type in four movies ");
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                movies[i] = Console.ReadLine();
+            }            
+
+            Console.WriteLine("\nHere they are alphabetically: ");
+            Array.Sort(movies); //Arrange the array in an alphabetically order
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine(movies[i]);
+            }
+
+            //Wait before closing
+            Console.ReadKey();  
+        }
+    }
+}
