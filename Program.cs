99a3044c0fc654f87ea9a12cb51891cb1fa6d6f6
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-04 09:34:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace My_Awesome_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numberGen1 = new Random(); //Define a number generator
+            Random numberGen2 = new Random();
+
+            int roll1 = 0;
+            int roll2 = 1; //Can't be zero because the program will start with the two of a kind already
+            int attempts = 0;
+
+            Console.WriteLine("Press enter to roll the dice.");
+
+            while(roll1 != roll2)
+            {
+                Console.ReadKey(); //Wait for the user to press the key before doing the loop
+                
+                roll1 = numberGen1.Next(1, 7); 
+                roll2 = numberGen2.Next(1, 7);
+                Console.WriteLine("Roll 1: " + roll1);
+                Console.WriteLine("Roll 2: " + roll2 + "\n");
+                attempts++; 
+            }
+
+            Console.WriteLine("It took you " + attempts + " attempts to roll two of a kind");
+            
+            //Wait before closing
+            Console.ReadKey();  
+        }
+    }
+}
