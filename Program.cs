13c785cede54dd75147a8e3b525b4455206221d8
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-05 10:14:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace My_Awesome_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("How many students are in your class: ");
+            
+            int students = Convert.ToInt32(Console.ReadLine());
+            string[] Students = new string[students];
+            
+            Console.WriteLine("Type in their names: ");
+
+            for (int i = 0; i < Students.Length; i++)
+            {
+                Students[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nHere they are alphabetically");
+            Array.Sort(Students);
+
+            for (int i = 0; i < Students.Length; i++)
+            {
+                Console.WriteLine(Students[i]);
+            }
+            
+            //Wait before closing
+            Console.ReadKey();  
+        }
+    }
+}
