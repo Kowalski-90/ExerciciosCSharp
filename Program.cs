952aff2a7126a81cb55750da9460d15d400e14ae
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-07 17:06:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace My_Awesome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            MeetAlien();            
+            Console.WriteLine("-------------");
+            MeetAlien();
+
+            //Wait before closing
+            Console.ReadKey();
+        }
+
+        static void MeetAlien()
+        {
+            Random numberGen = new Random();
+            string name = "X-" + numberGen.Next(10, 9999);
+            int age = numberGen.Next(10, 500);
+
+            Console.WriteLine("Hi, I'm " + name);
+            Console.WriteLine("I'm " + age + " years old.");
+            Console.WriteLine("Oh! And I'm an alien.");
+        }
+    }
+}
