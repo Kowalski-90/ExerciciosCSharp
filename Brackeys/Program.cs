850d6f90f66315c3df095a32ac15871c0e80176c
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-07-07 17:38:22 -0300

    Add files via upload

---
+++ Brackeys/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace My_Awesome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                       
+            Console.WriteLine("Put in your phrase");
+
+            string str = Console.ReadLine();
+
+            Console.WriteLine("There are " + StrLenght(str) + " words in that sentence.");
+
+            //Wait before closing
+            Console.ReadKey();
+        }
+
+        static int StrLenght(string str)
+        {
+            int lenght = str.Split(' ').Length;
+            return lenght;
+        }     
+    }
+}
